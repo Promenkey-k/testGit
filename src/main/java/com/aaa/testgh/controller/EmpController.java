935c4baf3d81d453b0ba27c8876eef34f9011f07
Promenkey-k <15660003543@163.com>
2018-11-29 09:38:40 +0800

    添加了EmpController

---
+++ src/main/java/com/aaa/testgh/controller/EmpController.java
@@ -0,0 +1,13 @@
+package com.aaa.testgh.controller;
+
+/**
+ * @className:EmpController
+ * @discription:
+ * @author:ProMonkey-K
+ * @creatTime:2018-11-29 09:34
+ */
+public class EmpController {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
