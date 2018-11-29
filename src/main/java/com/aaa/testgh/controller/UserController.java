3bf836600066a54f6be4303c9110d45767e5cfd4
Promenkey-k <15660003543@163.com>
2018-11-29 09:56:00 +0800

    添加User

---
+++ src/main/java/com/aaa/testgh/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.aaa.testgh.controller;
+
+/**
+ * @className:UserController
+ * @discription:
+ * @author:ProMonkey-K
+ * @creatTime:2018-11-29 09:55
+ */
+public class UserController {
+}
